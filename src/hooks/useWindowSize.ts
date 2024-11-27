4bbfa3c803ce3bce7da446643a03bc17af2d0422
AndyMelone <meloneandy225@gmail.com>
2024-11-27 02:38:51 +0000

    fix: fix bug for window in prod

--- src/hooks/useWindowSize.ts
+++ src/hooks/useWindowSize.ts
@@ -1,12 +1,18 @@
 import { useState, useEffect } from "react";
+import { useIsClient } from "./useIsClient";
 
 const useWindowSize = () => {
+  const isClient = useIsClient();
+
+  // Valeurs par défaut initiales pour éviter les erreurs côté serveur
   const [windowSize, setWindowSize] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight,
+    width: 0, // Valeurs par défaut sûres
+    height: 0,
   });
 
   useEffect(() => {
+    if (!isClient) return; // Ne s'exécute pas côté serveur
+
     const handleResize = () => {
       setWindowSize({
         width: window.innerWidth,
@@ -14,10 +20,15 @@ const useWindowSize = () => {
       });
     };
 
+    // Définir la taille initiale au montage
+    handleResize();
+
+    // Ajouter l'écouteur d'événements
     window.addEventListener("resize", handleResize);
 
+    // Nettoyage de l'écouteur lors du démontage
     return () => window.removeEventListener("resize", handleResize);
-  }, []);
+  }, [isClient]); // Dépendance à `isClient`
 
   return windowSize;
 };
